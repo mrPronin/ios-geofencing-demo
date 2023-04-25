17effc6aed529338f9902c2b1f52c157c6963106
Oleksandr Pronin <pronin.alx@gmail.com>
2023-04-25 15:07:28 +0200

    feat: add scene delegate

--- ios-geofencing-demo/GeofencingDemoAppDelegate.swift
+++ ios-geofencing-demo/GeofencingDemoAppDelegate.swift
@@ -12,4 +12,14 @@ class GeofencingDemoAppDelegate: NSObject, UIApplicationDelegate {
         print("Your code here")
         return true
     }
+    
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+      ) -> UISceneConfiguration {
+        let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        sceneConfig.delegateClass = GeofencingDemoSceneDelegate.self
+        return sceneConfig
+      }
 }
---
+++ ios-geofencing-demo/GeofencingDemoSceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  GeofencingDemoSceneDelegate.swift
+//  ios-geofencing-demo
+//
+//  Created by Pronin Oleksandr on 25.04.23.
+//
+
+import UIKit
+
+class GeofencingDemoSceneDelegate: NSObject, UIWindowSceneDelegate {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        print("scene(:willConnectTo)")
+    }
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // ...
+    }
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // ...
+    }
+    func sceneWillResignActive(_ scene: UIScene) {
+        // ...
+    }
+}
