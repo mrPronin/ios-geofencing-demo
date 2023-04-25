0105a688ce578452404f1323877b1e59ef817bd4
Oleksandr Pronin <pronin.alx@gmail.com>
2023-04-25 14:45:13 +0200

    Initial commit

---
+++ ios-geofencing-demo/GeofencingDemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  GeofencingDemoApp.swift
+//  ios-geofencing-demo
+//
+//  Created by Pronin Oleksandr on 25.04.23.
+//
+
+import SwiftUI
+
+@main
+struct GeofencingDemoApp: App {
+    @UIApplicationDelegateAdaptor(GeofencingDemoAppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-geofencing-demo/GeofencingDemoAppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  GeofencingDemoAppDelegate.swift
+//  ios-geofencing-demo
+//
+//  Created by Pronin Oleksandr on 25.04.23.
+//
+
+import UIKit
+
+class GeofencingDemoAppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Your code here")
+        return true
+    }
+}
